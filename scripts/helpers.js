a863e51f7efe45df99e6f30b15b71dc260e2a1ca
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2015-09-29 12:02:08 -0400

    added basic movements

---
+++ scripts/helpers.js
@@ -0,0 +1,49 @@
+fabric.Object.prototype.setOriginToCenter = function () {
+    this._originalOriginX = this.originX;
+    this._originalOriginY = this.originY;
+
+    var center = this.getCenterPoint();
+
+    this.set({
+        originX: 'center',
+        originY: 'center',
+        left: center.x,
+        top: center.y
+    });
+};
+
+fabric.Object.prototype.setCenterToOrigin = function () {
+    var originPoint = this.translateToOriginPoint(
+    this.getCenterPoint(),
+    this._originalOriginX,
+    this._originalOriginY);
+
+    this.set({
+        originX: this._originalOriginX,
+        originY: this._originalOriginY,
+        left: originPoint.x,
+        top: originPoint.y
+    });
+};
+
+
+function rotateObject(obj, angleOffset) {
+    var resetOrigin = false;
+
+    if (!obj) return;
+
+    var angle = obj.getAngle() + angleOffset;
+
+    if ((obj.originX !== 'center' || obj.originY !== 'center') && obj.centeredRotation) {
+        obj.setOriginToCenter && obj.setOriginToCenter();
+        resetOrigin = true;
+    }
+
+    obj.setAngle(angle).setCoords();
+
+    if (resetOrigin) {
+        obj.setCenterToOrigin && obj.setCenterToOrigin();
+    }
+
+    scope.renderAll();
+}
